42bc1ec2e1d4f2418a3322566951c139f94bb553
Shaz-M <shazmaradya@yahoo.com>
2023-04-17 14:13:08 -0500

    First commit

---
+++ src/components/prompt.js
@@ -0,0 +1,14 @@
+import '../App.css';
+
+function Prompt(){
+    return (
+        <div className="promptDiv">
+            <span className="promptTitle">Prompt</span>
+            <div className="prompt">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquet bibendum enim facilisis gravida. Eu sem integer vitae justo eget magna fermentum iaculis. Eu tincidunt tortor aliquam nulla facilisi cras fermentum odio. Amet est placerat in egestas erat imperdiet sed euismod. Blandit volutpat maecenas volutpat blandit aliquam. Quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus urna.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Prompt;
---
+++ src/helpers/walls.js
@@ -0,0 +1,20 @@
+import wall_1 from '../assets/wall1.png'
+import wall_2 from '../assets/wall2.png'
+import wall_3 from '../assets/wall3.png'
+import wall_4 from '../assets/wall4.png'
+
+export const roomWalls = [
+    {
+      image: wall_1
+    },
+    {
+      image: wall_2
+    },
+    {
+      image: wall_3
+    },
+    {
+      image: wall_4
+    },
+  ]
+  
