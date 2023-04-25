02e5a3083bbebc2ec857163b8e355f70f4b8201a
Joshua Strickland <strick9743@gmail.com>
2023-04-24 19:21:25 -0500

    welcome page created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import Game from './components/game.js'
+import React, { useState } from 'react';
+import './App.css';
+import WelcomePage from './components/welcomePage';
+import Game from './components/game';
 
 function App() {
+  const [showWelcome, setShowWelcome] = useState(true);
+
+  const startGame = () => {
+    setShowWelcome(false);
+  };
+
   return (
-      <Game/>
+    <div className="App">
+      {showWelcome ? (
+        <WelcomePage onStart={startGame} />
+      ) : (
+        <Game />
+      )}
+    </div>
   );
 }
 
--- src/components/welcomePage.js
+++ src/components/welcomePage.js
@@ -1,13 +1,14 @@
+// src/components/WelcomePage.js
 import React from 'react';
-import './WelcomePage.css';
+import './welcomePage.css';
 
-const WelcomePage = () => {
+const WelcomePage = ({ onStart }) => {
   return (
     <div className="welcome-page">
       <div className="welcome-container">
         <h1 className="welcome-title">Virtual Escape Room</h1>
         <p className="welcome-subtitle">Unravel the mysteries and find your way out!</p>
-        <button className="welcome-button">Start the Adventure</button>
+        <button className="welcome-button" onClick={onStart}>Start the Adventure</button>
       </div>
     </div>
   );
